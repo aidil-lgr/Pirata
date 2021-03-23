e93da8bc08dc409de9775e2c3513a9b613f50c15
Lídia García Romero <garciarl@campusemav.com>
2021-03-23 17:04:48 +0100

    23 / 17:04
    He arreglat coses el mode de joc i revisat player

--- Assets/Scripts/Scr Personatges/ScrPlayer.cs
+++ Assets/Scripts/Scr Personatges/ScrPlayer.cs
@@ -2,72 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 /// <summary>
 /// ----------------------------------------------------------------------------------
 /// DESCRIPCIÓ
 ///         Script utilitzat per controlar el player 
 /// AUTOR:  Elisabet Arnal
 /// DATA:   17/03/2021
-/// VERSIÓ: 1.0
+/// VERSIÓ: 3.0
 /// CONTROL DE VERSIONS
 ///         1.0: Moviment player i dispar(Aquest ultim no funciona)
 ///         2.0: Programació tripleshot. El dispar basic i el triple funcionen. 
+///         3.0: Lidia: correcció del bug que no sonava l'audio clip de disparar. Neteja de codi.
 /// ----------------------------------------------------------------------------------
 /// </summary>
 
 
-public class ScrPlayer : MonoBehaviour
-  
+public class ScrPlayer : MonoBehaviour  
 {
-
-    [SerializeField] float velocitat;
-    
-    
-
+    [SerializeField] float velocitat;      
     Vector2 movi = new Vector2(); //Calcul moviment
-    Rigidbody2D rb;               //Per accedir al riggidbody
-    [SerializeField] AudioSource BasicShot;
+    Rigidbody2D rb;
 
-    //**************Dispar*************
+    //Per disparar_____________________________________________________________
     [SerializeField] GameObject missil;
     [SerializeField] Transform[] canons;
-    
+    [SerializeField] AudioClip BasicShot;
 
-    //*************Dispars Seguits**************
-
-    [SerializeField] float cadencia = 0.5f; //Dispararà cada 5 dècimes de segón
+    [SerializeField] float cadencia = 0.5f; //Dispararà cada 5 dècimes de segon
     float crono = 0f;
-    float cronoPowerUp = 0;
+    float cronoPowerUp = 0f;
+    //_________________________________________________________________________
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        BasicShot = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Moviment amb joystic_____________________________________________________
         movi.x = ETCInput.GetAxis("Horizontal") * velocitat;
         movi.y = ETCInput.GetAxis("Vertical") * velocitat;
-        if (ETCInput.GetButton("Shoot") && crono > cadencia) Dispar();
+        //_________________________________________________________________________
+
+        //Disparar_________________________________________________________________
+        if (ETCInput.GetButton("Shoot") && crono > cadencia) Dispar(); //permet disparar amb cadència si el botó es queda clicat
 
         crono += Time.deltaTime;
 
         if (ETCInput.GetButtonUp("Shoot")) crono = cadencia; //parmet disparar ràpid amb diversos clics
 
-        if (Input.GetKeyDown(KeyCode.T))  // Prototipus triple shoot
-        {
-            DisparTriple(true);
-            cronoPowerUp = 5;
-        }
-        if (cronoPowerUp > 0) cronoPowerUp -= Time.deltaTime; else DisparTriple(false);
+        //if (Input.GetKeyDown(KeyCode.T)) 
+        //{
+            //DisparTriple(true);
+           // cronoPowerUp = 5;
+        //}
+        //if (cronoPowerUp > 0) cronoPowerUp -= Time.deltaTime; else DisparTriple(false);
 
     }
 
@@ -85,8 +76,7 @@ public class ScrPlayer : MonoBehaviour
             } 
         crono = 0;
 
-        BasicShot.Play();
-        
+        AudioSource.PlayClipAtPoint(BasicShot, Camera.main.transform.position);
     }
 
 
@@ -94,10 +84,6 @@ public class ScrPlayer : MonoBehaviour
     {
         canons[0].gameObject.SetActive(estat);
         canons[2].gameObject.SetActive(estat);
-
-        
-
-
     }
 
     void Detruccio() 
